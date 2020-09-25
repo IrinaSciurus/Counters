de321213bd11c721bedd31db0f54ee182f9152e5
IrinaSciurus <47363975+IrinaSciurus@users.noreply.github.com>
2020-09-25 19:55:51 +0300

    Updates

---
+++ precache-manifest.ccdfc266fa6d776f3ae30b8437443138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbe6881025cd0747a39f0efe64a5968",
+    "url": "/Counters/index.html"
+  },
+  {
+    "revision": "a02aff21da74cdffae38",
+    "url": "/Counters/static/css/main.5bea4d5d.chunk.css"
+  },
+  {
+    "revision": "223357a72cef16ea9fe3",
+    "url": "/Counters/static/js/2.f35ce4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counters/static/js/2.f35ce4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02aff21da74cdffae38",
+    "url": "/Counters/static/js/main.5008da7d.chunk.js"
+  },
+  {
+    "revision": "2de10c4255ab7db158ef",
+    "url": "/Counters/static/js/runtime-main.1cfbcc2d.js"
+  }
+]);
